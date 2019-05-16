79f8301b372419be9f94a0c485a52f69ec473df4
Ethan Miller <ethan.romans5.8@gmail.com>
2019-05-16 14:34:32 -0500

    add order functionality

--- northwind.py
+++ northwind.py
@@ -7,7 +7,7 @@ copyright = "Copyright © 2019 Collin College All rights reserved."
 
 @app.route("/")
 def index():
-   urls = {"products":url_for("products")}    
+   urls = {"products":url_for("products"),"suppliers":"/suppliers","orders":"/orders"}    
    return render_template("index.html", urls = urls,footer = copyright)
 
 # PRODUCTS
@@ -39,13 +39,13 @@ def product_details(id):
 
    cursor = db.cursor() 
  
-   cursor.execute('''
+   cursor.execute(f'''
         SELECT id,product_code,product_name,description,standard_cost,
         list_price,reorder_level, target_level,
         quantity_per_unit,discountinued,minimum_reorder_quantity,category,attachments
         FROM products
-        WHERE id = {0} 
-        '''.format(id))
+        WHERE id = {id}
+        ''')
 
    row = cursor.fetchone() 
     
@@ -82,14 +82,55 @@ def supplier_details(id):
 
    cursor = db.cursor() 
  
-   cursor.execute('''
+   cursor.execute(f'''
          SELECT id,company, last_name, first_name
          FROM suppliers
-         WHERE id = {0} 
-         '''.format(id))
+         WHERE id = {id}
+         ''')
 
    row = cursor.fetchone() 
     
    db.close()
    
    return render_template("supplier_detail.html", supplier = row, footer = copyright)
+
+# ORDERS
+
+@app.route("/orders")
+def orders():
+ 
+   db = sqlite3.connect("northwind2.db")
+
+   cursor = db.cursor() 
+ 
+   cursor.execute('''
+         SELECT id,order_date
+         FROM orders
+         ''')
+
+   # Get all rows.
+   rows = cursor.fetchall()
+
+   db.close()
+
+   # Pass rows into Jinja2 render_template function to use in orders.html.
+   return render_template("orders.html", urls = {"index":url_for("index")}, orders = rows, footer = copyright)
+
+@app.route("/orders/<int:id>")
+def order_details(id):
+
+   db = sqlite3.connect("northwind2.db")
+
+   cursor = db.cursor() 
+ 
+   cursor.execute(f'''
+            SELECT id,order_date
+            FROM orders
+            WHERE id = {id}
+            ''')
+
+   row = cursor.fetchone() 
+    
+   db.close()
+   
+   return render_template("order_detail.html", order = row, footer = copyright)
